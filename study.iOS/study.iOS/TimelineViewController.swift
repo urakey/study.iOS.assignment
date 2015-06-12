90117414357d88e6fcd74dbf1c374a759eebd8fa
Aki Fukayama <aki.fukayama@dena.com>
2015-06-12 18:45:22 +0900

    Init UITableViewController

---
+++ study.iOS/study.iOS/TimelineViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TimelineViewController.swift
+//  study.iOS
+//
+//  Created by Aki Fukayama on 2015/06/12.
+//  Copyright (c) 2015年 Chocolateboard. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TimelineViewController: UITableViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
