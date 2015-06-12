95caab959d154b34c053e523e35872bebdae389f
Aki Fukayama <aki.fukayama@dena.com>
2015-06-12 23:19:43 +0900

    Update table view

--- study.iOS/study.iOS/TimelineViewController.swift
+++ study.iOS/study.iOS/TimelineViewController.swift
@@ -11,25 +11,47 @@ import UIKit
 
 class TimelineViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var itemes = ["ちょこれーと", "こーひー", "いちご", "おそば"]
+    @IBOutlet var tableView: UITableView!
     
+    // Dummy data
+    var itemes = [
+        "ちょこれーと",
+        "こーひー",
+        "いちご",
+        "おそば",
+        "あいす",
+        "こうちゃ",
+        "けーき",
+        "ぎょうざ",
+        "ぱすた",
+        "かにくりーむころっけ",
+    ]
+    
+    // view 読み込み後
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // MEMO: storyboard で設定するの？コードで設定するの？
+        tableView.delegate = self;
+        tableView.dataSource = self;
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+
+    // MARK: セルの行数
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return itemes.count
     }
+
     
+    // MARK: セルの表示
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         // let cell = tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
-        let cell = tableView.dequeueReusableCellWithIdentifier("datas") as! UITableViewCell
-
-        cell.textLabel!.text = "てすと"
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        cell.textLabel?.text = itemes[indexPath.row]
         
         return cell
     }
